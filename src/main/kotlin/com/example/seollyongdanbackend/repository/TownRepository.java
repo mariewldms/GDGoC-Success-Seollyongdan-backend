46f0d50adbbf6ddbeac555a5c2ff91448866e1e3
ha9eun <ha9eunsoo@gmail.com>
2025-02-11 23:33:27 +0900

    Fix: TownRepository Long으로 변경

--- src/main/kotlin/com/example/seollyongdanbackend/repository/TownRepository.java
+++ src/main/kotlin/com/example/seollyongdanbackend/repository/TownRepository.java
@@ -3,5 +3,5 @@ package com.example.seollyongdanbackend.repository;
 import com.example.seollyongdanbackend.entity.Town;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TownRepository extends JpaRepository<Town, Integer> {
+public interface TownRepository extends JpaRepository<Town, Long> {
 }
