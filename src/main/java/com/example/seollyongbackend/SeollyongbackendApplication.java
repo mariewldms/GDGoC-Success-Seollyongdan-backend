d435770986f1f47b97fc23f269e7031b3947dd3a
leejieun <mariewldms@naver.com>
2025-02-13 17:06:07 +0900

    Feat: filtering csv 파일 추가

--- src/main/java/com/example/seollyongbackend/SeollyongbackendApplication.java
+++ src/main/java/com/example/seollyongbackend/SeollyongbackendApplication.java
@@ -1,8 +1,11 @@
 package com.example.seollyongbackend;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//배치 사용
+@EnableBatchProcessing
 @SpringBootApplication
 public class SeollyongbackendApplication {
 
--- src/main/java/com/example/seollyongbackend/controller/PreferController.java
+++ src/main/java/com/example/seollyongbackend/controller/PreferController.java
@@ -1,9 +1,19 @@
 package com.example.seollyongbackend.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
 public class PreferController {
+
+    //@PostMapping("/preference")
+    //post 받아서 네 가지 요소와 일치하는 clustering 번호 알아내고
+    //cluster 테이블의 id와 일치하는 값 response로 보내기
+
+    //@PutMapping("/preference")
+    //post와 동일한 흐름
+
 }
