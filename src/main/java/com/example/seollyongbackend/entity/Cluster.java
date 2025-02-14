1127a69a84fcebb41f8c4d9f6d85ff761ee81373
leejieun <mariewldms@naver.com>
2025-02-15 00:14:42 +0900

    Feat: cluster batch

--- src/main/java/com/example/seollyongbackend/entity/Cluster.java
+++ src/main/java/com/example/seollyongbackend/entity/Cluster.java
@@ -10,6 +10,9 @@ public class Cluster {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long cluster_id;
 
+//    @Column(nullable=false)
+//    private String cluster_id;
+
     @Column(nullable = false)
     private String safety;
 
@@ -22,20 +25,51 @@ public class Cluster {
     @Column(nullable = false)
     private String amenities;
 
+    @Column(nullable = true)
+    private String description;
+
     @Column(nullable = false)
     private String town;
 
     @Column(nullable = false)
     private String animal;
 
+    @Column(nullable=false)
+    private String animal_description;
+
     public Cluster(){}
 
-    public Cluster(String safety, String traffic, String real_estate, String amenities, String town, String animal) {
+    public Cluster(Long cluster_id, String safety, String traffic, String real_estate, String amenities, String description, String town, String animal, String animal_description) {
+        this.cluster_id = cluster_id;
         this.safety = safety;
         this.traffic = traffic;
         this.real_estate = real_estate;
         this.amenities = amenities;
+        this.description = description;
         this.town = town;
         this.animal = animal;
+        this.animal_description = animal_description;
+
     }
+
+    // Getters & Setters
+    public Long getCluster_id() { return cluster_id; }
+    public String getSafety() { return safety; }
+    public String getTraffic() { return traffic; }
+    public String getReal_estate() { return real_estate; }
+    public String getAmenities() { return amenities; }
+    public String getDescription() { return description; }
+    public String getTown() { return town; }
+    public String getAnimal() { return animal; }
+    public String getAnimal_description() { return animal_description; }
+
+    public void setCluster_id(Long cluster_id) { this.cluster_id = cluster_id; }
+    public void setSafety(String safety) { this.safety = safety; }
+    public void setTraffic(String traffic) { this.traffic = traffic; }
+    public void setReal_estate(String real_estate) {this.real_estate = real_estate; }
+    public void setAmenities(String amenities) {this.amenities = amenities; }
+    public void setDescription(String description) { this.description = description; }
+    public void setTown(String town) { this.town = town; }
+    public void setAnimal(String animal) { this.animal = animal; }
+    public void setAnimal_description(String animal_description) { this.animal_description = animal_description; }
 }
--- src/main/java/com/example/seollyongbackend/scheduler/CsvBatchScheduler.java
+++ src/main/java/com/example/seollyongbackend/scheduler/CsvBatchScheduler.java
@@ -1,6 +1,7 @@
 package com.example.seollyongbackend.scheduler;
 
 import com.example.seollyongbackend.service.CsvBatchService;
+import com.example.seollyongbackend.service.CsvClusterBatchService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -8,14 +9,23 @@ import org.springframework.stereotype.Component;
 public class CsvBatchScheduler {
 
     private final CsvBatchService csvBatchService;
+    private final CsvClusterBatchService csvClusterBatchService;
 
-    public CsvBatchScheduler(CsvBatchService csvBatchService) {
+    public CsvBatchScheduler(CsvBatchService csvBatchService, CsvClusterBatchService csvClusterBatchService) {
         this.csvBatchService = csvBatchService;
+        this.csvClusterBatchService = csvClusterBatchService;
     }
 
     @Scheduled(fixedRate = 36000000) // Config에서 값 가져오기
     public void runCsvBatch() {
-        System.out.println("CSV 배치 실행 중...");
+        //System.out.println("CSV 배치 실행 중...");
         csvBatchService.processCsv();
+        csvClusterBatchService.processCsv();
     }
+
+//    @Scheduled(fixedRate= 36000000)
+//    public void runClusterBatch() {
+//        //System.out.println("CSV 배치 실행 중...");
+//        csvClusterBatchService.processCsv();
+//    }
 }
